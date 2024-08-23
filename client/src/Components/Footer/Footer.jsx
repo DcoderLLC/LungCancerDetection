906b8f1aa90359396179b60a8ce7d357f4945d89
DcoderLLC <dcoderllc@gmail.com>
2024-08-23 23:02:35 +0530

    Navbar and footer done

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,9 +1,54 @@
-import {} from 'react'
+import { Box } from "@mui/material";
+import { Link } from "react-router-dom";
+import {} from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Box>
+      <footer className="bg-gray-900">
+        <div className="container px-6 py-8 mx-auto">
+          <div className="flex flex-col items-center text-center">
+            <div className="flex flex-wrap justify-center mt-2 -mx-4">
+              <Link
+                to="/"
+                className="mx-4 text-sm  transition-colors duration-300  text-white font-semibold hover:text-blue-500"
+                aria-label="Reddit"
+              >
+                {" "}
+                Home{" "}
+              </Link>
 
-export default Footer
+              <Link
+                to="/about"
+                className="mx-4 text-sm  transition-colors duration-300  text-white font-semibold hover:text-blue-500"
+                aria-label="Reddit"
+              >
+                {" "}
+                About{" "}
+              </Link>
+
+              <Link
+                to="/team"
+                className="mx-4 text-sm  transition-colors duration-300  text-white font-semibold hover:text-blue-500"
+                aria-label="Reddit"
+              >
+                {" "}
+                Teams{" "}
+              </Link>
+            </div>
+          </div>
+
+          <hr className="my-6 md:my-10 border-gray-700" />
+
+          <div className="flex flex-col items-center sm:flex-row sm:justify-between">
+            <p className="text-sm text-white">
+              © Copyright 2021. All Rights Reserved.
+            </p>
+          </div>
+        </div>
+      </footer>
+    </Box>
+  );
+};
+
+export default Footer;
--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,103 @@
-import {} from 'react'
+import { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { Box } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  // State to manage the navbar's visibility
+  const [nav, setNav] = useState(false);
 
-export default Navbar
+  // Toggle function to handle the navbar's display
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  const closeMenuOnMobile = () => {
+    if (window.innerWidth <= 1150) {
+      setNav(false);
+    }
+  };
+
+  return (
+    <Box>
+      <div className="bg-[#141E46] flex justify-between items-center h-24 w-full mx-auto px-4 text-white relative z-50">
+        {/* Logo */}
+        <Link to="/">
+          <h1 className="p-4 font-extrabold text-lg hover:bg-[#7D7C7C] hover:underline underline-offset-8 decoration-rose-600  m-2 cursor-pointer duration-500">
+            NTCC Project
+          </h1>
+        </Link>
+
+        {/* Desktop Navigation */}
+        <ul className="hidden md:flex">
+          <Link
+            to="/"
+            className="p-4 font-extrabold text-lg hover:bg-[#7D7C7C] hover:underline underline-offset-8 decoration-rose-600  m-2 cursor-pointer duration-500"
+          >
+            Home
+          </Link>
+
+          <Link
+            to="/about"
+            className="p-4 font-extrabold text-lg hover:bg-[#7D7C7C] hover:underline underline-offset-8 decoration-rose-600  m-2 cursor-pointer duration-500"
+          >
+            About
+          </Link>
+          <Link
+            to="/team"
+            className="p-4 font-extrabold text-lg hover:bg-[#7D7C7C] hover:underline underline-offset-8 decoration-rose-600  m-2 cursor-pointer duration-500"
+          >
+            Team
+          </Link>
+        </ul>
+
+        {/* Mobile Navigation Icon */}
+        <div onClick={handleNav} className="block md:hidden">
+          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        </div>
+
+        {/* Mobile Navigation Menu */}
+        <ul
+          className={
+            nav
+              ? "fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#141E46] ease-in-out duration-500 z-50"
+              : "ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%] z-50"
+          }
+        >
+          {/* Mobile Logo */}
+          <h1 className="p-4 font-extrabold text-lg hover:bg-[#7D7C7C] hover:underline underline-offset-8 decoration-rose-600  m-2 cursor-pointer duration-500">
+            NTCC Project
+          </h1>
+
+          {/* Mobile Navigation Items */}
+          <ul className="grid grid-rows-1">
+            <Link
+              onClick={closeMenuOnMobile}
+              to="/"
+              className="p-4 font-extrabold text-lg hover:bg-[#7D7C7C] hover:underline underline-offset-8 decoration-rose-600  m-2 cursor-pointer duration-500"
+            >
+              Home
+            </Link>
+
+            <Link
+              onClick={closeMenuOnMobile}
+              to="/about"
+              className="p-4 font-extrabold text-lg hover:bg-[#7D7C7C] hover:underline underline-offset-8 decoration-rose-600  m-2 cursor-pointer duration-500"
+            >
+              About
+            </Link>
+            <Link
+              onClick={closeMenuOnMobile}
+              to="/team"
+              className="p-4 font-extrabold text-lg hover:bg-[#7D7C7C] hover:underline underline-offset-8 decoration-rose-600  m-2 cursor-pointer duration-500"
+            >
+              Team
+            </Link>
+          </ul>
+        </ul>
+      </div>
+    </Box>
+  );
+};
+
+export default Navbar;
