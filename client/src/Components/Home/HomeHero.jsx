140b5e309c59d6e3e483ca8a430a2149e1d3040c
DcoderLLC <dcoderllc@gmail.com>
2024-08-24 14:00:25 +0530

    Updates

--- client/src/Components/Home/HomeHero.jsx
+++ client/src/Components/Home/HomeHero.jsx
@@ -6,10 +6,10 @@ const HomeHero = () => {
     <Box className="bg-gray-900">
       <div className="container px-6 py-16 mx-auto text-center">
         <div className="max-w-lg mx-auto">
-          <h1 className="text-3xl font-semibold text-gray-800 dark:text-white sm:text-4xl lg:text-5xl">
+          <h1 className="text-3xl font-semibold text-white sm:text-4xl lg:text-5xl">
             Building Your Next App with Our Awesome Components
           </h1>
-          <p className="mt-6 text-gray-500 dark:text-gray-300 sm:text-lg">
+          <p className="mt-6 text-gray-300 sm:text-lg">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero
             similique obcaecati illum mollitia.
           </p>
