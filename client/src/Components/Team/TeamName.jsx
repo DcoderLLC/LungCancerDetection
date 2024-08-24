545834ddbbba163b408243f03d8f3418812ab541
DcoderLLC <dcoderllc@gmail.com>
2024-08-24 13:39:54 +0530

    Team Page Updated

--- client/src/Components/Team/TeamName.jsx
+++ client/src/Components/Team/TeamName.jsx
@@ -1,29 +1,30 @@
 import { Box } from "@mui/material";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
-import nik from "../../assets/nik.jpg"
+import nik from "../../assets/nik.jpg";
+import vijay from "../../assets/vijay.jpg"
 
 const TeamName = () => {
   return (
     <Box>
-      <section className="bg-white dark:bg-gray-900">
-        <div className="container px-6 py-8 mx-auto">
-          <h2 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">
+      <section className="bg-white">
+        <div className="container px-6 py-8 m-8 mx-auto">
+          <h2 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl">
             Our Team
           </h2>
 
-          <div className="grid gap-8 mt-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <div className="grid gap-8 mt-8 sm:grid-cols-2 lg:grid-cols-3">
             {teamData.map((member, index) => (
-              <div key={index} className="w-full max-w-xs text-center">
+              <div key={index} className="w-full max-w-xs mx-auto text-center">
                 <img
                   className="object-cover object-center w-full h-72 mx-auto rounded-lg"
                   src={member.photo}
                   alt={member.name}
                 />
-                <div className="mt-2">
-                  <h3 className="text-lg font-medium text-gray-700 dark:text-gray-200">
+                <div className="mt-4">
+                  <h3 className="text-lg font-medium text-gray-700">
                     {member.name}
                   </h3>
-                  <span className="mt-1 font-medium text-gray-600 dark:text-gray-300">
+                  <span className="mt-1 font-medium text-gray-600 ">
                     {member.position}
                   </span>
                 </div>
@@ -33,7 +34,7 @@ const TeamName = () => {
                       href={member.linkedin}
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400"
+                      className="text-gray-600 hover:text-blue-600"
                     >
                       <FaLinkedin size={24} />
                     </a>
@@ -43,7 +44,7 @@ const TeamName = () => {
                       href={member.github}
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-400"
+                      className="text-gray-600 hover:text-gray-800"
                     >
                       <FaGithub size={24} />
                     </a>
@@ -77,10 +78,9 @@ const teamData = [
   {
     name: "Vijay Hursh",
     position: "Data Scientist",
-    photo:
-      "https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=731&q=80",
-    linkedin: "https://www.linkedin.com/in/steveben",
-    github: "https://github.com/steveben",
+    photo: vijay,
+    linkedin: "https://www.linkedin.com/in/vijay-hursh-267278251",
+    github: "https://github.com/Vijay-Hursh1590",
   },
 ];
 
